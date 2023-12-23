58d23d17c1505ae8916d132c966a2ed7d15524bc
VIJAY-MERNDEVELOPER <vijay.spm.07@gmail.com>
2023-12-23 18:53:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// selecting fields from the form
+
+let submit = document.querySelector(".btn");
+let firstName = document.querySelector("#first-name");
+let LastName = document.querySelector("#last-name");
+let gender = document.querySelector("input[name='Gender']:checked");
+let address = document.querySelector("#address");
+let city = document.querySelector("#inputCity");
+let state = document.querySelector("#inputState");
+let country = document.querySelector("#inputCountry");
+let pincode = document.querySelector("#pincode");
+let choiceOfFood = document.querySelectorAll("[name='cof']:checked");
+let table = document.getElementById("tableset");
+let tableBody = document.getElementById("table-body");
+let tablerow = document.querySelectorAll("tr");
+
+// calling submit button with addeventlistener when clicking button
+submit.addEventListener("click", function getDetails(e) {
+  let foodItems = [];
+  e.preventDefault();
+
+  for (let index = 0; index < choiceOfFood.length; index++) {
+    const element = choiceOfFood[index].value;
+    foodItems.push(element);
+  }
+  foodItems = foodItems.join(",");
+
+  tableBody.innerHTML += ` <tr>
+  
+<td>${firstName.value}</td>
+<td>${LastName.value}</td>
+<td>${address.value}</td>
+<td>${pincode.value}</td>
+<td>${gender.value}</td>
+<td>${foodItems}</td>
+<td>${state.value}</td>
+<td>${country.value}</td>
+</tr>`;
+
+  firstName.value = " ";
+  LastName.value = " ";
+  address.value = " ";
+  pincode.value = " ";
+  choiceOfFood;
+  state.value = "Choose...";
+  city.value = " ";
+  country.value = " ";
+});
